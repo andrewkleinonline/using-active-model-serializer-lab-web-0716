37b6e02fa5dea46c1d430967090c7dc91982970e
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-15 23:06:59 -0400

    complete lab

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,16 +2,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  # 
+  # def inventory
+  #   product = Product.find(params[:id])
+  #   render plain: product.inventory > 0 ? true : false
+  # end
 
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
+  # def description
+  #   product = Product.find(params[:id])
+  #   render plain: product.description
+  # end
 
   def new
     @product = Product.new
